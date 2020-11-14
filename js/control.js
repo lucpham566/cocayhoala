b96ed2e0834cebd8fd3fe32ea393aad9b46f4368
pham tien luc <lucpham566@gmail.com>
2020-11-14 18:11:56 +0700

    complete

--- js/control.js
+++ js/control.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
   window.addEventListener("scroll", function () {
     var obj = $("header.header-menu").offset().top;
-    console.log(obj);
     if (obj > 50) {
       $("header.header-menu").addClass("scroll");
+      $(".addThis_listSharing").css({
+        display:"block"
+      })
     } else {
       $("header.header-menu").removeClass("scroll");
+      $(".addThis_listSharing").css({
+        display:"none"
+      })
     }
   });
 });
@@ -13,15 +18,15 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".carousel-banner").slick({
     infinite: true,
-    dots:true,
+    dots: true,
     speed: 700,
     autoplay: true,
     autoplaySpeed: 2000,
   });
-  
+
   $(".carousel-testimonier").slick({
     infinite: true,
-    dots:false,
+    dots: false,
     speed: 700,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -31,33 +36,80 @@ $(document).ready(function () {
         breakpoint: 1008,
         settings: {
           slidesToShow: 2,
-        }
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-        }
-      }
-
-    ]
+        },
+      },
+    ],
   });
-  
+
   $(".carousel-special-products").slick({
     infinite: true,
-    dots:false,
+    dots: false,
     speed: 700,
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 1,
-
   });
-  
+
+  $(".carousel-gallery").slick({
+    infinite: true,
+    dots: true,
+    speed: 700,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
   $(".tab-a").click(function () {
     $(".tab").removeClass("tab-active");
     $(".tab[data-id='" + $(this).attr("data-id") + "']").addClass("tab-active");
     $(".tab-a").removeClass("active-a");
     $(this).parent().find(".tab-a").addClass("active-a");
   });
-});
 
+  var isSearch = false;
+
+  $("main").on("click", function () {
+    isSearch = false;
+    $("header .search .content").css({
+      display: "none",
+    });
+  });
+  $(".banner-top").on("click", function () {
+    isSearch = false;
+    $("header .search .content").css({
+      display: "none",
+    });
+  });
+  $("header .search .btn-sch").on("click", function () {
+    isSearch = !isSearch;
+    if (isSearch) {
+      $("header .search .content").css({
+        display: "block",
+      });
+    } else {
+      $("header .search .content").css({
+        display: "none",
+      });
+    }
+  });
+});
